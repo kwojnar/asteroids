0968dff814ae4be14b2f5193b47e9b3a9122e34e
kwojnar <kwojnar@akamai.com>
2024-12-05 23:00:40 +0100

    added moving around

--- main.py
+++ main.py
@@ -20,7 +20,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+
+        player.update(dt)
         player.draw(screen)
+        
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
